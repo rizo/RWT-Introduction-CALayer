a0faa3fe923a97fadf4c7cb239d8ebc6daf0c736
ravero <developer@ravero.net>
2013-01-04 17:52:33 -0200

    Add basic sublayer example.

--- LayerFun/LayerFunViewController.m
+++ LayerFun/LayerFunViewController.m
@@ -22,6 +22,15 @@
     self.view.layer.backgroundColor = [UIColor orangeColor].CGColor;
     self.view.layer.cornerRadius = 20.0;
     self.view.layer.frame = CGRectInset(self.view.layer.frame, 20, 20);
+    
+    CALayer *sublayer = [CALayer layer];
+    sublayer.backgroundColor = [UIColor blueColor].CGColor;
+    sublayer.shadowOffset = CGSizeMake(0, 3);
+    sublayer.shadowRadius = 5.0;
+    sublayer.shadowColor = [UIColor blackColor].CGColor;
+    sublayer.shadowOpacity = 0.8;
+    sublayer.frame = CGRectMake(30, 30, 128, 192);
+    [self.view.layer addSublayer:sublayer];
 }
 
 - (void)didReceiveMemoryWarning
